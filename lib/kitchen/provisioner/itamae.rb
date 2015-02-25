1f00bb69f5e38673e166ba32d0ce4d5bdfb51f67
sawanoboly <sawanoboriyu@higanworks.com>
2015-02-25 20:40:55 +0900

    converge success

--- lib/kitchen/provisioner/itamae.rb
+++ lib/kitchen/provisioner/itamae.rb
@@ -1,13 +1,100 @@
 require 'json'
 require "kitchen-itamae/version"
 require 'kitchen/provisioner/base'
+require "kitchen/util"
 
 module Kitchen
   module Provisioner
     class Itamae < Base
-       default_config :require_itamae_omnibus, false
-       default_config :itmae_omnibus_url, nil
+      # TODO: should not parasite chef
+      default_config :parasite_chef_omnibus, true
+      default_config :chef_omnibus_url, "https://www.chef.io/chef/install.sh"
+      default_config :chef_omnibus_root, "/opt/chef"
+      default_config :chef_omnibus_install_options, nil
 
-   end
+      default_config :itamae_root, "kitchen"
+      default_config :recipe_list, []
+      default_config :node_json, nil
+
+      # (see Base#create_sandbox)
+      def create_sandbox
+        super
+#        prepare_data
+#        prepare_script
+        FileUtils.cp_r(Dir.glob("kitchen/*"), sandbox_path)
+      end
+
+      def cleanup_sandbox
+        nil
+      end
+
+      # (see Base#run_command)
+      def run_command
+        runlist = config[:config][:recipe_list].map do |recipe|
+          cmd = ["cd #{config[:root_path]};", "sudo" , 'itamae']
+          cmd << 'local'
+          cmd << "-j #{config[:config][:node_json]}" if config[:config][:node_json]
+          cmd << recipe
+          cmd.join(" ")
+        end
+        Util.wrap_command(runlist.join("\n"))
+      end
+
+
+      # (see ChefBase#install_command)
+      def install_command
+        return unless config[:parasite_chef_omnibus]
+        lines = [Util.shell_helpers, download_helpers, chef_helpers, chef_install_function, itamae_install_function]
+        Util.wrap_command(lines.join("\n"))
+      end
+
+      private
+      # (see ChefBase private)
+
+      def download_helpers
+        IO.read(
+          Gem.find_files_from_load_path('../support/download_helpers.sh').first
+        )
+      end
+
+      def chef_helpers
+        IO.read(
+          Gem.find_files_from_load_path('../support/chef_helpers.sh').first
+        )
+      end
+
+      def chef_install_function
+        version = config[:parasite_chef_omnibus].to_s.downcase
+        pretty_version = case version
+                         when "true" then "install only if missing"
+                         when "latest" then "always install latest version"
+                         else version
+                         end
+        install_flags = %w[latest true].include?(version) ? "" : "-v #{version}"
+        if config[:chef_omnibus_install_options]
+          install_flags << " " << config[:chef_omnibus_install_options]
+        end
+        <<-INSTALL_CHEF.gsub(/^ {10}/, "")
+          if should_update_chef "#{config[:chef_omnibus_root]}" "#{version}" ; then
+          echo "-----> Installing Chef Omnibus (#{pretty_version})"
+          do_download #{config[:chef_omnibus_url]} /tmp/install.sh
+                  #{sudo("sh")} /tmp/install.sh #{install_flags.strip}
+          else
+          echo "-----> Chef Omnibus installation detected (#{pretty_version})"
+          fi
+        INSTALL_CHEF
+      end
+
+      def itamae_install_function
+        <<-INSTALL_ITAMAE
+         cat <<EOL > /tmp/install_itamae.sh
+         [ -f /usr/local/bin/itamae ] && exit
+         /opt/chef/embedded/bin/gem install itamae --no-ri --no-rdoc
+         ln -sf /opt/chef/embedded/bin/itamae /usr/local/bin/
+EOL
+         #{sudo("sh")} /tmp/install_itamae.sh
+        INSTALL_ITAMAE
+      end
+    end
   end
 end
